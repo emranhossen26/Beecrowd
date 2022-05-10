dd210fdfcaf129373a5ad6ee1111ded630fbca97
Md. Emran Hossen <92653363+emranhossen26@users.noreply.github.com>
2022-05-11 00:33:57 +0600

    Add files via upload

---
+++ Online Judge.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,c,result;
+    scanf("%d %d %d",&a,&b,&c);
+    result=(a+b+abs(a-b))/2;
+    result=(c+result+abs(c-result))/2;
+
+    printf("%d eh o maior",result);
+
+    return 0;
+}
